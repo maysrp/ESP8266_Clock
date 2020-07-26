b3a2aa00f821e11f90ee48530e6da63590fab23c
Xiaosea <maysrp@gmail.com>
2020-07-26 08:07:47 +0800

    Update boot.py

--- boot.py
+++ boot.py
@@ -2,66 +2,44 @@ import time
 import ntptime
 import network
 import max7219
-from machine import Pin,SPI
+from machine import Pin,SPI,RTC
 class clock:
     def __init__(self):
         self.ntp()
         self.dp()
         self.se=0
-        self.time_zone=8 #your Time zone
-        self.host='ntp.aliyun.com' #ntp server
+        self.rtc=RTC()
     def net(self):
         wlan = network.WLAN(network.STA_IF) 
         wlan.active(True) 
         if not wlan.isconnected(): 
-            wlan.connect('Your_wifi','Your_wifi_password')
+            wlan.connect('wifiname','password')
     def dp(self):
         hspi = SPI(1, baudrate=10000000, polarity=0, phase=0)
         self.display = max7219.Matrix8x8(hspi,Pin(5),4)
     def ntp(self):
         self.net()
-        ntptime.host=self.host
+        ntptime.host="ntp1.aliyun.com"
+        ntptime.NTP_DELTA = 3155644800
         try:
             ntptime.settime()
         except Exception as e:
             pass
-    def text(self,texts):
+    def show_time(self):
+        date=self.rtc.datetime()
+        self.m=date[5]
+        self.h=date[4]
         self.display.fill(0)
-        self.display.text(texts[0:2],0,0,1)
+        self.display.text(str(self.h) if len(str(self.h))==2 else ' '+str(self.h) ,0,0,1)
         self.display.pixel(16,2,self.se)        
         self.display.pixel(16,4,self.se)        
-        self.display.text(texts[2:4],17,0,1)
+        self.display.text(str(self.m) if len(str(self.m))==2 else '0'+str(self.m) ,17,0,1)
         self.se=0 if self.se==1 else 1
         self.display.show()
-    def show_time(self):
-        ti=time.localtime()
-        h=ti[3]+self.time_zone if ti[3]+self.time_zone<24 else ti[3]+self.time_zone-24
-        h=h if h>9 else '0'+str(h)
-        m=ti[4]
-        m=m if m>9 else '0'+str(m)
-        s=ti[5]
-        w=ti[6]+1
-        self.h=ti[3]
-        del ti
-        time_string=str(h)+str(m)+'Day'+str(w)
-        self.text(time_string)
-    def show_time_s(self):
-        ti=time.localtime()
-        h=ti[3]+8
-        h=h if h>9 else '0'+str(h)
-        m=ti[4]
-        m=m if m>9 else '0'+str(m)
-        s=ti[5]
-        s=s if s>9 else '0'+str(s)
-        w=ti[6]+1
-        self.m=ti[4]
-        del ti
-        time_string=str(h)+str(m)+'__'+str(s)
-        self.text(time_string)
 Clock=clock()
 
 while 1:
-    Clock.show_time_s()
+    Clock.show_time()
     time.sleep(1)
     if Clock.m==10:
         Clock.ntp()
